9c707e33773ddc56996f925285154f880abe7545
Nour-Nasr <nourkn01@gmail.com>
2021-04-15 15:44:09 +0300

    update Culture recipes script Relates #18

--- CultureRecipes/CultureRecipes.js
+++ CultureRecipes/CultureRecipes.js
@@ -1,26 +1,28 @@
 const searchBtn = document.getElementById('search-button');
 const mealList = document.getElementById('meal');
 
-
+searchBtn.addEventListener('click', getMealList)
 // get the meal list depend on area
 
-function getMealList(){
+function getMealList(e){
+    e.preventDefault()
     let searchInput = document.getElementById('search-input').value.trim();
+    console.log(searchInput)
     fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?a=${searchInput}`)
     .then(response => response.json())
     .then(data => {
-        e.preventDefault();
+        console.log(data)
         if(data.meals){
             data.meals.forEach(meal => {
                 const mealItem = document.createElement('div')
-                mealItem.setAttribute('id', '${meal.idMeal}')
+                mealItem.setAttribute('id', meal.idMeal )
                 mealItem.classList.add('meal-item')
 
                 const mealImgContainer = document.createElement('div')
                 mealImgContainer.classList.add('meal-img')
 
                 const mealimg = document.createElement("img")
-                mealimg.setAttribute('src' , '${meal.strMealThumb}')
+                mealimg.setAttribute('src' , meal.strMealThumb)
                 mealimg.setAttribute('alt' , 'meal')
                 mealImgContainer.appendChild(mealimg)
 
@@ -28,17 +30,18 @@ function getMealList(){
                 mealName.classList.add('meal-name')
 
                 const headername = document.createElement('h3')
-                headername.textContent = '${meal.strMeal}'
+                headername.textContent = meal.strMeal
                 mealName.appendChild(headername)
 
                 mealItem.appendChild(mealImgContainer)
                 mealItem.appendChild(mealName)
+                mealList.appendChild(mealItem)
+                console.log(mealItem)
             });
         } else{
             html = "Sorry, we didn't find any meal by this area!";
             mealList.classList.add('notFound');
         }
 
-        mealList.appendChild(mealItem)
 }   )};
     
